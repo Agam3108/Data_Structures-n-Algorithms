e51e7e08ca3b4032123df5cf7977f439f1528356
Agam <118785408+Agam3108@users.noreply.github.com>
2024-05-11 17:20:35 +0530

    Add files via upload

---
+++ 1Knapsack.cpp
@@ -0,0 +1,19 @@
+ int knapSack(int W, int wt[], int val[], int n) 
+    { 
+       // Your code here
+      vector<vector<int>> dp(n,vector<int>(W+1,0));
+       for(int i=wt[0];i<=W;i++){
+           dp[0][i]=val[0];
+       }
+       for(int i=1;i<n;i++){
+           for(int j=0;j<=W;j++){
+               int notTake=dp[i-1][j];
+               int take=INT_MIN;
+               if(wt[i]<=j)
+               take=val[i]+dp[i-1][j-wt[i]];
+               dp[i][j]=max(take,notTake);
+           }
+           
+       }
+       return dp[n-1][W];
+    }
