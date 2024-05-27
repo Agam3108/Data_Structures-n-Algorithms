006bd5703014b84e08bfa8c748a7c2c0e96a0571
Agam <118785408+Agam3108@users.noreply.github.com>
2024-05-27 14:44:08 +0530

    Add files via upload

---
+++ Buy&SellStock2.cpp
@@ -0,0 +1,20 @@
+int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<int> curr(2,0);
+        vector<int> ahead(2,0);
+        ahead[0]=ahead[1]=0;
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<=1;buy++){
+                int profit=0;
+                if(buy){
+                    profit=max(-prices[ind]+ahead[0],ahead[1]);
+                }
+                else{
+                    profit=max(prices[ind]+ahead[1],ahead[0]);
+                }
+                curr[buy]=profit;
+            }
+            ahead=curr;
+        }
+     return ahead[1];
+    }
