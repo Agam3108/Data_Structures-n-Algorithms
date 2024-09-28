3d2dcab16f62c6ddee68cfa85e8dd1d2a2b0173c
Agam <118785408+Agam3108@users.noreply.github.com>
2024-09-28 10:39:08 +0530

    Time: 113 ms (5.04%), Space: 385.1 MB (5%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void combination2(vector<vector<int>>& res, vector<int>& combination,
+                      vector<int>& candidates, int target, int index) {
+        if (target == 0) {
+            res.push_back(combination);
+            return;
+        }
+        for (int i = index; i < candidates.size() && target >= candidates[i];
+             ++i) {
+            if (i == index || candidates[i] != candidates[i - 1]) {
+                combination.push_back(candidates[i]);
+                combination2(res, combination, candidates,
+                             target - candidates[i], i + 1);
+                combination.pop_back();
+            }
+        }
+        }
+        vector<vector<int>> combinationSum2(vector<int> & candidates,
+                                            int target) {
+            int n = candidates.size();
+            sort(candidates.begin(), candidates.end());
+            vector<vector<int>> dp(1000, vector<int>(1000 + 1));
+            vector<int> ds;
+            vector<vector<int>> temp;
+            combination2(temp,ds,candidates,target,0);
+            // for (auto it : ds) {
+            //     cout << it << " ";
+            // }
+
+            // for (auto it : ans) {
+            //     temp.push_back(it);
+            // }
+            return temp;
+        }
+    };
