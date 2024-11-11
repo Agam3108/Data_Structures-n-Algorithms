2cbec7f8885f4737d8b09aa64fae4647764e4298
Agam <118785408+Agam3108@users.noreply.github.com>
2024-11-11 17:17:34 +0530

    Time: 19 ms (16.06%), Space: 29 MB (31.95%) - LeetHub

---
+++ 2716-prime-subtraction-operation/2716-prime-subtraction-operation.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+vector<int> SieveOfEratosthenes(int n)
+{
+    vector<bool> prime(n + 1, true);
+      vector<int> res;
+  for (int p = 2; p * p <= n; p++) {
+        if (prime[p] == true) {
+            for (int i = p * p; i <= n; i += p)
+                prime[i] = false;
+        }
+    }
+    for (int p = 2; p <n; p++) {
+        if (prime[p])
+           res.push_back(p);
+    }
+    return res;
+}
+ bool isIncreasingArray(const vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 1; i < n; i++) {
+            if (nums[i - 1] >= nums[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    bool primeSubOperation(vector<int>& nums) {
+          int n = nums.size();
+        vector<int> primes = SieveOfEratosthenes(1000); 
+        
+        for (int i = 0; i < n; i++) {
+            bool valid = false;
+            for (int j = primes.size() - 1; j >= 0; j--) {
+                if (primes[j] < nums[i] && (i == 0 || nums[i] - primes[j] > nums[i - 1])) {
+                    nums[i] -= primes[j];
+                    valid = true;
+                    break;
+                }
+            }
+            if (!valid && i > 0 && nums[i] <= nums[i - 1]) {
+                return false; 
+            }
+        }
+
+        return true;
+    }
+};
