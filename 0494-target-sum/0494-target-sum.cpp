c2039c63fc158b897fe9042718a727632c394443
Agam <118785408+Agam3108@users.noreply.github.com>
2024-12-26 09:52:57 +0530

    Time: 4 ms (81.21%), Space: 15 MB (47.55%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+
+     int findWays(vector<int> &num, int tar){
+     int n = num.size();
+
+    vector<int> prev(tar+1,0);
+    
+    if(num[0] == 0) prev[0] =2;  
+    else prev[0] = 1; 
+    
+    if(num[0]!=0 && num[0]<=tar) prev[num[0]] = 1; 
+    
+    for(int ind = 1; ind<n; ind++){
+        vector<int> cur(tar+1,0);
+        for(int target= 0; target<=tar; target++){
+            int notTaken = prev[target];
+    
+            int taken = 0;
+                if(num[ind]<=target)
+                    taken = prev[target-num[ind]];
+        
+            cur[target]= (notTaken + taken);
+        }
+        prev = cur;
+    }
+    return prev[tar];
+}
+
+    int countPartitions(int n, int d, vector<int>& arr) {
+        // Code here
+        int totSum=0;
+        for(int i=0;i<n;i++){
+            totSum+=arr[i];
+        }
+        if(totSum-d<0 || (totSum-d)%2)
+        return false;
+        return findWays(arr,(totSum-d)/2);
+    }
+
+
+    int findTargetSumWays(vector<int>& nums, int target) {
+       
+    int n=nums.size();
+    return countPartitions(n,target,nums);
+    }
+};
