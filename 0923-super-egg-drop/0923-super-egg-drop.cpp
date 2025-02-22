6b5024ac3f407a3d4fc21389efa340239614843a
Agam <118785408+Agam3108@users.noreply.github.com>
2025-02-22 17:30:04 +0530

    Time: 35 ms (78.05%), Space: 12.4 MB (71.64%) - LeetHub

--- 0923-super-egg-drop/0923-super-egg-drop.cpp
+++ 0923-super-egg-drop/0923-super-egg-drop.cpp
@@ -12,10 +12,19 @@ public:
             {  
         int ans = INT_MAX;
         int lo = 1;int hi = n;
+        int b = 0;int nb = 0;
            while(lo <= hi){
             int mid = (lo+hi)/2;
-            int b = solve(k-1,mid-1);
-            int nb = solve(k,n-mid);
+            if(t[k-1][mid-1]!=-1)
+             b = t[k-1][mid-1];
+            else
+             b = solve(k-1,mid-1);
+            t[k-1][mid-1] = b;
+            if(t[k][n-mid]!=-1)
+             nb = t[k][n-mid];
+            else
+             nb = solve(k,n-mid);
+            t[k][n-mid] = nb;
             ans = min(ans,1+max(b,nb));
             if(b > nb)
             hi = mid-1;
