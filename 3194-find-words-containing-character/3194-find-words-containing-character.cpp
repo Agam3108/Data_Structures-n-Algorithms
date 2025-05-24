b666ca1d55470920bbe185645218961844cf8669
Agam <118785408+Agam3108@users.noreply.github.com>
2025-05-24 08:34:00 +0530

    Time: 7 ms (5.91%), Space: 33.5 MB (20.07%) - LeetHub

---
+++ 3194-find-words-containing-character/3194-find-words-containing-character.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        int n = words.size();
+        vector<int> ans;
+        for(int i = 0;i<n;i++){
+            string word = words[i];
+            if(word.find(x,0) != string::npos){
+              ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
