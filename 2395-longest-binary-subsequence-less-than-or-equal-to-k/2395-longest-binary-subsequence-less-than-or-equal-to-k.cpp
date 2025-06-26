e6711e16b5bda9ff61dff05402ce9a53d5ca12a7
Agam <118785408+Agam3108@users.noreply.github.com>
2025-06-26 10:51:51 +0530

    Time: 12 ms (23.28%), Space: 13.4 MB (15.87%) - LeetHub

---
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/2395-longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+int dp[1001][1001];
+    int longestSubsequence(string s, int k) {
+        int n = s.length();
+        int countZeros = 0;
+        int count = 0;
+        int value = 0;
+        int power = 0;
+        for(int i = n-1;i>=0;i--){
+            char c = s[i];
+            if(c == '0'){
+            countZeros++;
+            power++;
+            }
+            else{
+                if(value + pow(2,power) <= k){
+                    value += pow(2,power);
+                    count++;
+                    power++;
+                }
+            }
+        }
+       return count + countZeros ;    
+    }
+};
